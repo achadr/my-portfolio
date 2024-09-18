9990b634668cbb124bb7aa50d82a77be0a890e36
Achraf <achraf.bougattaya@gmail.com>
2024-09-18 16:33:32 +0200

    project setup

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  let something = 2+2
+  console.log(something)
+  return (
+    <h1 >
+      Project from scratch
+    </h1>
+  );
+}
