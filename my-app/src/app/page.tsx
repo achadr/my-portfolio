f25e3f1f9b62a596008682805436d5b7cd259d7b
Achraf <achraf.bougattaya@gmail.com>
2024-09-19 18:26:49 +0200

    header and framer motion setup

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,11 +1,6 @@
-import Image from "next/image";
 
 export default function Home() {
-  let something = 2+2
-  console.log(something)
   return (
-    <h1 >
-      Project from scratch
-    </h1>
+    <main></main>
   );
 }
