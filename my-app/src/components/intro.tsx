3da649422067e611c204dbe3015bf3a0e3731737
Achraf <achraf.bougattaya@gmail.com>
2025-12-04 13:02:57 +0100

    Update experience description in Intro component to reflect 4+ years of front-end development experience.

--- my-app/src/components/intro.tsx
+++ my-app/src/components/intro.tsx
@@ -63,7 +63,7 @@ export default function Intro() {
             >
         <span className="font-bold">Hello, I&apos;m Achraf.</span> I&apos;m a{" "}
         <span className="font-bold">front-end developer</span> with{" "}
-        <span className="font-bold">3 years</span> of experience. I enjoy
+        <span className="font-bold">4+ years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My main focus is{" "}
         <span className="underline">React</span>.
         </motion.h1>
